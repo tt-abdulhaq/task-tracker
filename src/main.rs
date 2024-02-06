5b6f420cb5d678d01166d4bfa3dc6ebeb46d700d
tt-abdulhaq <abdularifi10am135@gmail.com>
2024-02-07 00:20:34 +0430

    implement user input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::{Write}};
 
 
 #[derive(Debug)]
@@ -70,29 +70,86 @@ impl TaskTrucker {
             );
         }
     }
-
-    fn display_task(self, name:&str){
-        if let Some(task) = self.tasks.get(name){
-            println!(
-                "{}: {:#?}\n",
-                name, task
-            );
-        }else {
-            println!("Task '{}' not found!", name);
-        }
-    }
-
-
-    
 }
 
 fn main(){
-    let mut tt1 = TaskTrucker::new();
-    tt1.add_task("task", "description", "due_date", Status::Pending);
-    tt1.add_task("task1", "description", "due_date", Status::Pending);
-    tt1.add_task("task2", "description", "due_date", Status::Pending);
-    tt1.update_task_status("task2", Status::Completed);
-    tt1.display_task("task22");
-    
-    
+    let mut task_tracker = TaskTrucker::new();
+    hint();
+    loop {
+        println!();
+        println!();
+        println!("##############################################################");
+        let choice = get_user_input("Enter your choice:");
+        let choice = match choice.parse::<u8>(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                0
+            }
+        };
+        handle_task(&mut task_tracker, choice)
+
+    }   
+}
+fn hint(){
+    println!("Task Tracker Menu:");
+    println!("1. Add Task");
+    println!("2. Update Task Status");
+    println!("3. Delete Task");
+    println!("4. Display Tasks");
+    println!("5. Exit");
+}
+
+fn add_task(task_trucker:&mut TaskTrucker){
+    let name = get_user_input("Enter task name: ");
+    let description = get_user_input("Enter task description: ");
+    let due_date = get_user_input("Enter task to do date: ");
+    task_trucker.add_task(&name, &description, &due_date, Status::Pending);
+}
+
+fn update_task_status(task_trucker:&mut TaskTrucker){
+    let name = get_user_input("Enter task name: ");
+    let status = get_user_input("Enter new status (0 for Pending, 1 for Completed): ");
+    let new_status = match status.parse::<u32>() {
+        Ok(0) => Status::Pending,
+        Ok(1) => Status::Completed,
+        _ => {
+            println!("Invalid status input. Setting status to Pending.");
+            Status::Pending
+        }
+    };
+    task_trucker.update_task_status(&name, new_status);
+}
+
+fn handle_task(task_trucker:&mut TaskTrucker, choice: u8){
+    match  choice { 
+        1 => {
+            add_task(task_trucker);
+        }
+        2 => {
+            update_task_status(task_trucker);
+        }
+        3 => {
+            let name = get_user_input("Enter task name: ");
+            task_trucker.delete_task(&name);
+        }
+        4 => {
+            task_trucker.display_tasks();
+        }
+        5 => {
+            std::process::exit(0);
+        }
+        _ => {
+            println!("Please enter a correct number!");
+            hint();
+        }
+    }
+}
+
+fn get_user_input(prompt: &str) -> String {
+    print!("{}", prompt);
+    std::io::stdout().flush().unwrap();
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Failed to read input");
+    input.trim().to_string()
 }
